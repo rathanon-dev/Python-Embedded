91b9d1fc400b7fe0fff56609527ff99d38989e5c
Jame <rathanon.dev@gmail.com>
2026-03-21 04:33:58 +0700

    Fix script path in RunAs.bat for PythonDev.ps1

--- RunAs.bat
+++ RunAs.bat
@@ -1,5 +1,6 @@
 @echo off
 REM check pwsh
+ 
 where pwsh >nul 2>&1
 if errorlevel 1 (
     echo PowerShell 7+ pwsh not found in PATH.
@@ -10,7 +11,7 @@ if errorlevel 1 (
 )
 
 REM run PythonNuget.ps1 elevated from the same folder as this batch
-set "scriptPath=%~dp0PythonDev.ps1"
+set "scriptPath=%~dp0\PythonDev.ps1"
 
 powershell -Command ^
     "Start-Process pwsh -ArgumentList '-NoProfile','-ExecutionPolicy Bypass','-NoExit','-Command & ''%scriptPath%'' ' -Verb RunAs"
